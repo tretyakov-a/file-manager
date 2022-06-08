bc54289dfa0bb5552916e9089e3da13a10846a1b
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-08 11:42:37 +0300

    refactor: remove excess code

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,28 +1,8 @@
 import Command from './command.js';
 import commandsInfo from './index.js';
-import { msg } from '../appearance.js';
-
-const toSnakeCase = (str) => {
-  let result = '';
-  for (const c of str) {
-    if (c === c.toUpperCase()) {
-      result += '_';
-    }
-    result += c.toLowerCase();
-  }
-  return result;
-};
-
-export const toCommandInfo = ({ name, argsConfig, description }) => {
-  const nameOutput = msg.dir(`${name} `);
-  const argsOutput = argsConfig.length !== 0
-    ? msg.hl(argsConfig.map(({ name }) => `<${toSnakeCase(name)}> `).join(''))
-    : '';
-  return `${nameOutput}${argsOutput}${description}`;
-}
 
 async function help() {
-  const data = commandsInfo.map(toCommandInfo);
+  const data = commandsInfo.map(Command.printCommandInfo);
   return this.onSuccess(undefined, data.join('\n'));
 }
 
