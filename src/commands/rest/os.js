6451a045b9bb0b290f16deff70fdb29dafe38a99
Alexander Tretyakov <tpetyakov@gmail.com>
2022-06-11 12:47:29 +0300

    refactor: os command

--- src/commands/rest/os.js
+++ src/commands/rest/os.js
@@ -2,51 +2,63 @@ import osNode from 'os';
 import Command from '../command.js';
 import { msg } from '../../appearance.js';
 
-const KEYS = {
-  EOL: '--EOL',
-  CPUS: '--cpus',
-  HOMEDIR: '--homedir',
-  USERNAME: '--username',
-  ARCHITECTURE: '--architecture',
-}
-
 const getCpusInfo = () => {
   const cpus = osNode.cpus();
   const results = cpus.map(({ model, speed}, index) => {
     return `#${index + 1} ${msg.hl('Model:')} ${model}; ${msg.hl('Speed:')} ${speed / 1000}GHz`;
   });
   return `Overall amount: ${results.length}: \n${results.join('\n')}`;
-}
+};
 
-const keysHandlers = {
-  [KEYS.EOL]: () => osNode.EOL === '\n' ? '\\n' : '\\r\\n',
-  [KEYS.CPUS]: getCpusInfo,
-  [KEYS.HOMEDIR]: () => osNode.userInfo().homedir,
-  [KEYS.USERNAME]: () => osNode.userInfo().username,
-  [KEYS.ARCHITECTURE]: () => osNode.platform(),
+const createKey = (description, handler) => {
+  return { description, handler };
+};
+
+const keys = {
+  ['--EOL']: createKey(
+    'Get EOL (default system End-Of-Line)',
+    () => osNode.EOL === '\n' ? '\\n' : '\\r\\n',
+  ),
+  ['--cpus']: createKey(
+    'Get host machine CPUs info (overall amount of CPUS plus model and clock rate (in GHz) for each of them)',
+    getCpusInfo,
+  ),
+  ['--homedir']: createKey(
+    'Get home directory',
+    () => osNode.userInfo().homedir,
+  ),
+  ['--username']: createKey(
+    'Get current system user name (Do not confuse with the username that is set when the application starts)',
+    () => osNode.userInfo().username,
+  ),
+  '--architecture': createKey(
+    'Get CPU architecture for which Node.js binary has compiled',
+    () => osNode.platform(),
+  ),
 };
 
 async function os() {
   const [ key ] = this.args;
   try {
-    const data = await keysHandlers[key].call(null);
+    const data = await keys[key].handler.call(null);
     return this.onSuccess(undefined, data);
   } catch (err) {
     this.onError(err);
   }
 }
 
+const printKeys = () => {
+  return Object.keys(keys)
+    .map((key) => `  ${msg.hl(key)} - ${keys[key].description}`)
+    .join('\n');
+};
+
 export default Command.createOptions(
   'os',
   [
     Command.createArg('key', Command.ARGS.KEY),
   ],
-  `Operating system info (prints following information in console)
-  ${KEYS.EOL} - Get EOL (default system End-Of-Line)
-  ${KEYS.CPUS} - Get host machine CPUs info (overall amount of CPUS plus model and clock rate (in GHz) for each of them)
-  ${KEYS.HOMEDIR} - Get home directory
-  ${KEYS.USERNAME} - Get current system user name (Do not confuse with the username that is set when the application starts)
-  ${KEYS.ARCHITECTURE} - Get CPU architecture for which Node.js binary has compiled`,
+  `Operating system info (prints following information in console)\n${printKeys()}`,
   os,
-  Object.values(KEYS),
+  Object.keys(keys),
 );
